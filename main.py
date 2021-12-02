57ceb6f4332b313edf0da85a85f4c80c610d52ba
aargullos <aargullos@institutmvm.cat>
2021-12-02 15:34:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    llista = list()
+    x = int(input("Introdueix el numero del que vols la llista: "))
+    for x in range(x):
+        list.append int(input("Introdueix els valors de la llista: "))
+    print(llista)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
